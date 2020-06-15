bd6aea83d5739b5108830b39a01768e255e7d3c2
Abylay Murzashev <abylay.murzashev@gmail.com>
2020-06-15 09:31:29 +0600

    feat: add eslint rules

--- src/components/App/types.ts
+++ src/components/App/types.ts
@@ -1,5 +1,5 @@
-import { ItemsState } from "src/duck/types/items";
-import { ConfigState } from "src/duck/types/config";
+import { ItemsState } from 'src/duck/types/items';
+import { ConfigState } from 'src/duck/types/config';
 
 export interface AppProps {
   loadConfig: () => void,
